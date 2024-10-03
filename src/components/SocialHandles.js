d90216d19be00ec84a9da0f50feb1c71deeeeee6
veena1232 <veeenanandeti@gmail.com>
2024-10-03 20:52:02 +0530

    some changes

--- src/components/SocialHandles.js
+++ src/components/SocialHandles.js
@@ -5,7 +5,7 @@ import { FaXTwitter } from "react-icons/fa6";
 const SocialHandles = () => {
   return (
     <div data-aos="zoom-in-up" data-aos-duration="1500" data-aos-once="false" className="flex gap-5 my-4">
-        <a className="text-white text-2xl md:text-xl transition duration-700 hover:scale-125" href="https://github.com/VijayKumarReddyTalakola" target="_blank" rel="noreferrer">
+        <a className="text-white text-2xl md:text-xl transition duration-700 hover:scale-125" href="https://github.com/veena1232" target="_blank" rel="noreferrer">
           <FaGithub />
         </a>
         <a className="text-white text-2xl md:text-xl transition duration-700 hover:scale-125" href="https://www.linkedin.com/in/veena24/" target="_blank" rel="noreferrer">
